8f5f9697d463abdb9b0b13b86f9085ca45d4a0e2
Bardia Namjoo <bardiademon@gmail.com>
2023-01-24 00:28:57 +0330

    Added btn

--- src/views/dashboardPage.vue
+++ src/views/dashboardPage.vue
@@ -5,6 +5,7 @@ import {computed} from "vue";
 const orders = computed(() => store.getters.getOrders);
 
 import store from "@/store";
+import router from "@/router";
 
 store.dispatch('getOrders');
 const getOrders = () =>
@@ -31,9 +32,19 @@ const removeOrder = (id) =>
       });
 };
 
+const changeRoute = (name) =>
+{
+  router.push(name);
+};
+
 </script>
 
 <template>
+  <br/>
+  <button @click="changeRoute('add-order')">Add Order</button>
+  <button @click="changeRoute('foods')">Foods</button>
+  <button @click="changeRoute('users')">Users</button>
+  <button @click="changeRoute('categories')">Categories</button>
   <br/>
   <table>
     <thead>
@@ -81,7 +92,7 @@ const removeOrder = (id) =>
         {{ order.created_at }}
       </td>
       <td>
-        <button @click="removeOrder(order.id)">Delete</button>
+        <button class="btn-delete" @click="removeOrder(order.id)">Delete</button>
       </td>
     </tr>
     </tbody>
@@ -98,5 +109,51 @@ table
 table, th, td
 {
   border: 1px solid;
+}
+
+
+/* CSS */
+button
+{
+  appearance: button;
+  backface-visibility: hidden;
+  background-color: #405cf5;
+  border-radius: 6px;
+  border-width: 0;
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .1) 0 2px 5px 0, rgba(0, 0, 0, .07) 0 1px 1px 0;
+  box-sizing: border-box;
+  color: #fff;
+  cursor: pointer;
+  font-family: -apple-system, system-ui, "Segoe UI", Roboto, "Helvetica Neue", Ubuntu, sans-serif;
+  font-size: 100%;
+  height: 44px;
+  line-height: 1.15;
+  margin: 15px;
+  outline: none;
+  overflow: hidden;
+  padding: 0 25px;
+  position: relative;
+  text-align: center;
+  text-transform: none;
+  transform: translateZ(0);
+  transition: all .2s, box-shadow .08s ease-in;
+  user-select: none;
+  -webkit-user-select: none;
+  touch-action: manipulation;
+}
+
+.btn-delete
+{
+  background-color: #FF4742;
+}
+
+button:disabled
+{
+  cursor: default;
+}
+
+button
+{
+  box-shadow: rgba(50, 50, 93, .1) 0 0 0 1px inset, rgba(50, 50, 93, .2) 0 6px 15px 0, rgba(0, 0, 0, .1) 0 2px 2px 0, rgba(50, 151, 211, .3) 0 0 0 4px;
 }
 </style>
